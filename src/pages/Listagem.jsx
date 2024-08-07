c965fad1760693217678d4aee4aa36ac5b130279
Brunimxz <nuzamaki266@gmail.com>
2024-08-07 13:46:51 -0300

    commit 1

---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,10 @@
+import React, {useState,useEffect} from 'react'
+
+const Listagem = () => {
+    return (
+        <>
+        <h2>Pizzariaa ITB</h2>
+        </>
+    )
+}
+export default Listagem
