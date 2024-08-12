4a4d152a7c773a0f9fd52754ce815b14c94c5f21
Brunimxz <nuzamaki266@gmail.com>
2024-08-12 08:39:30 -0300

    commit 8

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -3,7 +3,7 @@ import calabresa from "../assets/images/calabresa.png"
 import atum from "../assets/images/atum.png"
 import portuguesa from "../assets/images/portuguesa.png"
 import frango from "../assets/images/frango.png"
-
+ 
 const Pizzas = [
     {
       "id": 1,
@@ -36,5 +36,5 @@ const Pizzas = [
       "imagem": atum
     },
   ]
-
+ 
   export default Pizzas;
